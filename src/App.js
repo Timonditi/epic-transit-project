40e63fc0d5d8e01aa05df9d6f5a9fb3bfbf08a6f
rukia <ruqayashar1@gmail.com>
2023-06-06 00:54:35 +0300

    Bus.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import HomePage from './Components/HomePage';
+import Bus from './Components/Bus';
+import Footer from './Components/Footer';
+
+function App () {
+    return (
+        <div>
+            <h1>WELCOME TO EPIC TRANSIT</h1>
+            <HomePage />
+            <Bus />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
+
+
