0f56bcadfe65343b9aa3f0701dcc9c6d1d663e87
rukia <ruqayashar1@gmail.com>
2023-06-05 22:01:43 +0300

    HomePage.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="homePage">
+            <h2>EPIC TRANSIT</h2>
+        </div>
+    );
+}
+
+export default HomePage;
