94f1271515348c46e0ff5bd0ba5a402cd6bcd18a
Timothy <todongoonditi@gmail.com>
2023-06-05 19:02:20 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainBookingForm from './components/Train';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Epic Transit</h1>
+      <TrainBookingForm />
+    </div>
+  );
+};
+
+export default App;
