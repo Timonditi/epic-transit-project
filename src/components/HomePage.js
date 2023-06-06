edefb69b4469475e5c35be521e78634cb3d8d6dc
rukia <ruqayashar1@gmail.com>
2023-06-06 13:44:02 +0300

    Title

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,7 @@ import React from "react";
 function HomePage() {
     return (
         <div className="homePage">
+            <h1><b>EPIC TRANSIT</b></h1>
             <h2>Welcome to our online ticketing</h2>
         </div>
     );
