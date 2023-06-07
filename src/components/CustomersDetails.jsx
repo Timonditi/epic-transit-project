32dbe8868cf13fe6431bc38d04aaafb15d2ff2ae
Michal <khakaimic4@gmail.com>
2023-06-07 20:16:32 +0300

    post for customer details

--- src/components/CustomersDetails.jsx
+++ src/components/CustomersDetails.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function CustomerDetails() {
   const [firstName, setFirstName] = useState('');
@@ -26,55 +26,81 @@ function CustomerDetails() {
     setIdPassport(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleCreateCustomer = async (customer) => {
+    try {
+      const response = await fetch('http://localhost:9292/customers', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(customer),
+      });
+
+      if (response.ok) {
+        const createdCustomer = await response.json();
+        console.log( createdCustomer);
+        // Perform any necessary actions with the created customer
+      } else {
+        console.error(response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Perform any necessary actions with the customer data
-    // (e.g., submit to backend, validate, etc.)
+    // Fetch the existing data from the backend
+    try {
+      const response = await fetch('http://localhost:9292/customers');
+      if (response.ok) {
+        const customerData = await response.json();
+        console.log(customerData);
+        // Perform any necessary actions with the fetched data
 
-    // Navigate to a new route and pass customer details as URL parameters
-    navigate(`/payment-confirmation?firstName=${firstName}&lastName=${lastName}&phoneNumber=${phoneNumber}&idPassport=${idPassport}`);
+        // Create the customer object
+        const customer = {
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          idPassport: idPassport,
+        };
+
+        // Call the POST request handler
+        await handleCreateCustomer(customer);
+
+        // Navigate to a new route and pass customer details as URL parameters
+        navigate(
+          `/payment-confirmation?firstName=${firstName}&lastName=${lastName}&phoneNumber=${phoneNumber}&idPassport=${idPassport}`
+        );
+      } else {
+        console.error(response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
       <h2>Customer Details</h2>
-      <form onSubmit={handleSubmit} className='form'>
+      <form onSubmit={handleSubmit} className="form">
         <div>
           <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={handleFirstNameChange}
-          />
+          <input type="text" id="firstName" value={firstName} onChange={handleFirstNameChange} />
         </div>
         <div>
           <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={handleLastNameChange}
-          />
+          <input type="text" id="lastName" value={lastName} onChange={handleLastNameChange} />
         </div>
         <div>
           <label htmlFor="phoneNumber">Phone Number:</label>
-          <input
-            type="text"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-          />
+          <input type="text" id="phoneNumber" value={phoneNumber} onChange={handlePhoneNumberChange} />
         </div>
         <div>
           <label htmlFor="idPassport">ID/Passport:</label>
-          <input
-            type="text"
-            id="idPassport"
-            value={idPassport}
-            onChange={handleIdPassportChange}
-          />
+          <input type="text" id="idPassport" value={idPassport} onChange={handleIdPassportChange} />
         </div>
         <button type="submit">Confirm Payment</button>
       </form>
