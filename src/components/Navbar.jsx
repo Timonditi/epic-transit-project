20c94b3ca2f17c9a1d25129e5feb99bb33f30551
rukia <ruqayashar1@gmail.com>
2023-06-09 00:45:06 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
+
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav>
       <ul className="navbar-list">
-        <li className="navbar-item">
+        <li className={`navbar-item ${location.pathname === '/' ? 'active' : ''}`}>
           <Link to="/" className="navbar-link">Bus</Link>
         </li>
-        <li className="navbar-item">
+        <li className={`navbar-item ${location.pathname === '/Booking' ? 'active' : ''}`}>
           <Link to="/Booking" className="navbar-link">Train</Link>
         </li>
-        <li className="navbar-item">
+        <li className={`navbar-item ${location.pathname === '/CustomerReviews' ? 'active' : ''}`}>
           <Link to="/CustomerReviews" className="navbar-link">Reviews</Link>
         </li>
       </ul>
